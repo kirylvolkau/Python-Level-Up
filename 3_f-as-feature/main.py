ed1b70d875eac6a0a261a8d48d8466f0eb6eab73
Kiryl Volkau <volkaukiryl@gmail.com>
2020-04-26 16:15:58 +0200

    2nd task

--- 3_f-as-feature/main.py
+++ 3_f-as-feature/main.py
@@ -1,16 +1,7 @@
-import secrets
-from functools import wraps
 from hashlib import sha256
-from pydantic import BaseModel
-from requests import auth
-from fastapi import FastAPI, HTTPException, status, Response, Cookie, Request
-from fastapi.responses import JSONResponse
+from fastapi import FastAPI, HTTPException, status, Response, Cookie, Request, Depends
 from starlette.responses import RedirectResponse
-
-
-class UserLogin(BaseModel):
-	login: str
-	password: str
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
 
 def is_logged_in(f):
 	@wraps(f)
@@ -19,15 +10,16 @@ def is_logged_in(f):
 		session_token = Cookie(None)
 		if "session_token" in request.cookies:
 			session_token = request.cookies["session_token"]
-		if session_token != app.allowed_user_hash:
+		if session_token != app.allowed_hash:
 			raise HTTPException(status_code=401, detail='Not authorized')
 		return f(*args,**kwargs)
 	return wrapper
 			
 
 app = FastAPI()
-app.secret_key = "very constatn and random secret, best 64 characters"
-app.allowed_user_hash = sha256(bytes(f"trudnYPaC13Nt{app.secret_key}",encoding='utf8')).hexdigest()
+app.secret = "simple secret for the app"
+app.allowed_hash = sha256(bytes(f"trudnYPaC13Nt{app.secret}", encoding="utf8")).hexdigest()
+security = HTTPBasic()
 
 @app.get('/')
 def root():
@@ -37,17 +29,14 @@ def root():
 @app.get('/welcome/')
 @is_logged_in
 def welcome(request: Request, session_token: str = Cookie(None)):
-	return {"message": "Hello World during the coronavirus pandemic!"}
+	return {"msg": "Hello World during the coronavirus pandemic!"}
 
-@app.post('/login/')
-def login_user(request_model : UserLogin, response : Response):
-	user_hash = sha256(bytes(f"{request_model.login}{request_model.password}{app.secret_key}",encoding='utf8')).hexdigest()
-	print(user_hash)
-	print(app.allowed_user_hash)
-	# change to smth using normal encoding - decoding // user_hash = encode(request_model.login,request_model.password)
-	if app.allowed_user_hash != user_hash:
-		raise HTTPException(status_code=401,detail='Login failed.')
-	response = RedirectResponse( url='/welcome', status_code=status.HTTP_302_FOUND)
-	response.set_cookie(key="session_token", value=user_hash)
-	return response
 
+@app.post('/login')
+def lel(credentials : HTTPBasicCredentials = Depends(security)):
+	if credentials.username == "trudnY" and credentials.password == "PaC13Nt":
+		response = RedirectResponse(url='/welcome')
+		s_token = sha256(bytes(f"{credentials.username}{credentials.password}{app.secret}", encoding='utf8')).hexdigest()
+		return response
+	else:
+		raise HTTPException(status_code=500)
