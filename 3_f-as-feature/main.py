abdf85582c8229b3e0356ba21b4569f46b03000b
Kiryl Volkau <volkaukiryl@gmail.com>
2020-04-26 20:13:17 +0200

    task 5 finish

--- 3_f-as-feature/main.py
+++ 3_f-as-feature/main.py
@@ -1,15 +1,55 @@
 from hashlib import sha256
 from functools import wraps
+from pydantic import BaseModel
 from fastapi import FastAPI, HTTPException, Response, Cookie, Depends, Request
+from fastapi.responses import JSONResponse
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from fastapi.templating import Jinja2Templates
+
+import csv
+import pandas as pd
+
+class PatientToCreate(BaseModel):
+	name : str
+	surname : str 
+
+class PatientToReturn(BaseModel):
+	id: int
+	patient: PatientToCreate
+
+#get patient, create patient, 
+def get_id():
+	count = 0
+	with open('storage.csv') as storage:
+		reader = csv.reader(storage)
+		count = len(list(reader))-1
+	return count
+
+def find_patient(id: int):
+	df = pd.read_csv('storage.csv')
+	patient = df[df.id == id]
+	if patient.empty:
+		return None
+	tmp = PatientToCreate(name = patient.name.item(), surname = patient.surname.item())
+	return PatientToReturn(id = id, patient = tmp)
+
+def add_patient(patient : PatientToCreate):
+	insert = [get_id(), patient.name, patient.surname]
+	with open('storage.csv', 'a+') as storage:
+		csv_writer = csv.writer(storage)
+		csv_writer.writerow(insert)
+
+def reset():
+	with open('storage.csv', 'r') as fin:
+		data = fin.read().splitlines(True)
+	with open('storage.csv', 'w') as fout:
+		fout.writelines(data[0])
 
 
 def is_logged_in(func):
 	@wraps(func)
 	def inner(*args, **kwargs):
 		request = kwargs['request']
-		print(request.cookies)
 		if 'session_token' in request.cookies and request.cookies['session_token'] == app.allowed_user_hash:
 			return func(*args,**kwargs)
 		else:
@@ -19,6 +59,10 @@ def is_logged_in(func):
 
 LOGIN = 'trudnY'
 PASS = 'PaC13Nt'
+
+responses = dict()
+responses[200] = JSONResponse(status_code=200, content={"message" : "OK"})
+responses[204] =  Response(status_code=204)
 
 app = FastAPI()
 app.secret_key = 'Mizt14O3sE3AWCviiIkUuwqURvGEf2Tn'
@@ -52,4 +96,40 @@ def logout(request : Request, response : Response):
 	response.headers['Location'] = '/'
 	return response
 
+@app.get('/patient')
+@is_logged_in
+def get_patients(request : Request):
+	df = pd.read_csv('storage.csv')
+	answer = dict()
+	for index,row in df.iterrows():
+		answer[row['id']] = {"name":row['name'],"surname":row['surname']}
+	return answer
+
+@app.post('/patient')
+@is_logged_in
+def create_patient(patient : PatientToCreate, request:Request, response : Response):
+	add_patient(patient)
+	response.status_code = 302
+	response.headers['Location'] = f'/patient/{get_id()-1}'
+	return response
+
+@app.get('/patient/{id}')
+@is_logged_in
+def get_patient(id: int, request : Request):
+	patient = find_patient(id)
+	if patient:
+		return patient.patient
+	else:
+		return responses[204]
+
+@app.delete('/patient/{id}')
+@is_logged_in
+def delete_patient(id:int, request : Request):
+	df = pd.read_csv('storage.csv')
+	patient = df[df.id == id]
+	if not patient.empty:
+		df = df[df.id != id]
+		reset()
+		df.to_csv('storage.csv', index=False)
+	return responses[200]
 
